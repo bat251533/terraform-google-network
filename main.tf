95d23d5ccea6e05ba2fdb29b50bf2bbcd0c2bb18
bat251533 <112114084+bat251533@users.noreply.github.com>
2022-08-25 23:05:50 +0530

    Update main.tf
    vpc creation

--- main.tf
+++ main.tf
@@ -18,15 +18,66 @@
 	VPC configuration
  *****************************************/
 module "vpc" {
-  source                                 = "./modules/vpc"
-  network_name                           = var.network_name
-  auto_create_subnetworks                = var.auto_create_subnetworks
-  routing_mode                           = var.routing_mode
-  project_id                             = var.project_id
-  description                            = var.description
-  shared_vpc_host                        = var.shared_vpc_host
-  delete_default_internet_gateway_routes = var.delete_default_internet_gateway_routes
-  mtu                                    = var.mtu
+    source  = "terraform-google-modules/network/google"
+    version = "~> 4.0"
+
+    project_id   = "qwiklabs-gcp-02-afa1c5ae4c64"
+    network_name = "example-vpc"
+    routing_mode = "GLOBAL"
+
+    subnets = [
+        {
+            subnet_name           = "subnet-01"
+            subnet_ip             = "10.10.10.0/24"
+            subnet_region         = "us-west1"
+        },
+        {
+            subnet_name           = "subnet-02"
+            subnet_ip             = "10.10.20.0/24"
+            subnet_region         = "us-west1"
+            subnet_private_access = "true"
+            subnet_flow_logs      = "true"
+            description           = "This subnet has a description"
+        },
+        {
+            subnet_name               = "subnet-03"
+            subnet_ip                 = "10.10.30.0/24"
+            subnet_region             = "us-west1"
+            subnet_flow_logs          = "true"
+            subnet_flow_logs_interval = "INTERVAL_10_MIN"
+            subnet_flow_logs_sampling = 0.7
+            subnet_flow_logs_metadata = "INCLUDE_ALL_METADATA"
+        }
+    ]
+
+    secondary_ranges = {
+        subnet-01 = [
+            {
+                range_name    = "subnet-01-secondary-01"
+                ip_cidr_range = "192.168.64.0/24"
+            },
+        ]
+
+        subnet-02 = []
+    }
+
+    routes = [
+        {
+            name                   = "egress-internet"
+            description            = "route through IGW to access internet"
+            destination_range      = "0.0.0.0/0"
+            tags                   = "egress-inet"
+            next_hop_internet      = "true"
+        },
+        {
+            name                   = "app-proxy"
+            description            = "route through proxy to reach app"
+            destination_range      = "10.50.10.0/24"
+            tags                   = "app-proxy"
+            next_hop_instance      = "app-proxy-instance"
+            next_hop_instance_zone = "us-west1-a"
+        },
+    ]
 }
 
 /******************************************
